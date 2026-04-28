43d7a327d400c4b8c05c5d26d7fa01d927015698
fkngy26 <youjiefuyong174@gmail.com>
2026-04-28 21:06:54 +0900

    add css

---
+++ index/home_page.js
@@ -0,0 +1,19 @@
+console.log("a");
+let list1 = [1,2,3];
+console.log(list1[0])
+
+const test = (arg) =>{
+    console.log(arg);
+}
+
+for(let i=1;i<=3;i++){
+    test(3);
+}
+
+const obj = {
+    color:'pink',
+    size:'big',
+    strongth:3
+}
+
+console.log(obj.strongth);
