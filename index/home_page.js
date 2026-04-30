ef321a4f714b5eb6361c9bd214d4fff71ad194d1
fkngy26 <youjiefuyong174@gmail.com>
2026-05-01 02:12:30 +0900

    add task_list but it needs little improve

--- index/home_page.js
+++ index/home_page.js
@@ -1,19 +1,28 @@
-console.log("a");
-let list1 = [1,2,3];
-console.log(list1[0])
+let Tasks=["Get up","sleep","study"];
 
-const test = (arg) =>{
-    console.log(arg);
+const initialize=()=>{    
+    for(let i=0;i<Tasks.length;i++){
+        addTask(i);
+    }
+}
+// setAttributeを使用して、繰り返しながらもidを一意に定めることができるらしい
+// これにより、label.forが機能するようになる。
+// CSSでは改行をするのではなく、inline設定からblock設定で横並びから縦並びにすることができる。
+// ToDoの中のlabelにだけ、CSSを適用したいといった2つ以上の条件を適用したい差異は、CSSセレクタと呼ばれるものを理解する必要があるらしい。
+const addTask=(task_num)=>{
+    let task_list=document.getElementById("iToDoList");
+    let label=document.createElement("label");
+    label.for="task"
+    let checkbox=document.createElement("input");
+    checkbox.type="checkbox";
+    checkbox.id="task";
+    let text=document.createElement("span");
+    text.textContent=Tasks[task_num];
+
+    label.appendChild(checkbox);
+    label.appendChild(text);
+
+    task_list.appendChild(label);
 }
 
-for(let i=1;i<=3;i++){
-    test(3);
-}
-
-const obj = {
-    color:'pink',
-    size:'big',
-    strongth:3
-}
-
-console.log(obj.strongth);
+initialize();
