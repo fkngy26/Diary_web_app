a109d3631a57eb54a609ddac4813b08c79682bef
fkngy26 <youjiefuyong174@gmail.com>
2026-05-02 16:53:22 +0900

    May 2

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, jsonify
+import sqlite3
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    html=render_template('home_page.html')
-    return html
+# データベースに接続
+# def get_db_connection():
+#     conn=sqlite3.connect('/database/Diary.db')
+#     conn.row_factory=sqlite3.Row
+#     return conn
+
+# データの追加
+@app.route("/",methods=["POST"])
+def index():
+    # return "POST Method"
+    return render_template("/home_page.html")
 
 if __name__ == "__main__":
     app.run()
