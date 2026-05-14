a1a9fcea5cceee918349507ee1b0c84d46163bb6
fkngy26 <youjiefuyong174@gmail.com>
2026-05-14 10:34:15 +0900

    add diary db

--- flaskr/today.py
+++ flaskr/today.py
@@ -13,31 +13,41 @@ bp = Blueprint('today', __name__, url_prefix='/today')
 def writeTodayDiary():
   db=get_db()
   if request.method=='POST':
-    task=request.form.getlist('habit_ids')
-    error=None
-
     try:
-      placeholder=",".join("?"*len(task))
-      activeTasks=db.execute(
-        f"SELECT * FROM habit WHERE id IN ({placeholder})",
-        task
-      ).fetchall()
+      today=datetime.datetime.today().date()
+      task=request.form.getlist('habit_ids')
+      if task:
+        placeholder=",".join("?"*len(task))
+        activeTasks=db.execute(
+          f"SELECT * FROM habit WHERE id IN ({placeholder})",
+          task
+        ).fetchall()
 
-      task_objects=[]
-      for task in activeTasks:
-        task_object=[]
-        task_object.append(task['id'])
-        task_object.append(task['habit_name'])
-        task_object.append(datetime.datetime.today().date())
-        print(task_object)
-        task_objects.append(task_object)
-      print(task_objects)
-      
-      db.executemany(
-        "INSERT INTO habit_logs (id, habit_id, date) VALUES (?,?,?)",
+        # task_logはidで管理する。※同じ名前を使用しても問題ないように
+        task_objects=[]
+        for task in activeTasks:
+          task_object=[]
+          task_object.append(task['id'])
+          task_object.append(today)
+          task_objects.append(task_object)
+        db.executemany(
+          "INSERT INTO habit_logs ( habit_id, date) VALUES (?,?)",
           task_objects
-      )
+        )
+      diary_text=request.form.get('diary')
+      print(diary_text)
+      if diary_text:
+        db.execute(
+          "INSERT INTO diary ( date, diary_text) VALUES (?,?)",
+          (today,diary_text)
+        )
       db.commit()
+      diaries=db.execute(
+        "SELECT * FROM diary"
+      ).fetchall()
+      for d in diaries:
+        print(d['diary_text'])
+
     except Exception as e:
       print(str(e))
 
--- memo.md
+++ memo.md
@@ -1,7 +1,5 @@
 flaskr/__init__のSECRET='dev'の部分は、デプロイする際は変更する
 
-today.pyのDBに入れる部分を書いた
-      db.executemany(
-        "INSERT INTO habit_logs (id, habit_id, date) VALUES (?,?,?)",
-          task_objects
-      )
+DiaryがDBに追加できるように変更した。
+
+次はList.pyとそのページを作成する。
