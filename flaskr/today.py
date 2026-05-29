43c951da9091bf3e6432c815414e163f1d7e0f32
fkngy26 <youjiefuyong174@gmail.com>
2026-05-30 04:21:36 +0900

    add UPDATE function

--- flaskr/today.py
+++ flaskr/today.py
@@ -12,16 +12,12 @@ bp = Blueprint('today', __name__, url_prefix='/today')
 @bp.route('/writeTodayDiary',methods=['GET','POST'])
 def writeTodayDiary():
   db=get_db()
-  today=datetime.datetime.today().date()
-  if request.method=='POST':
-    try:
-      # POSTでは、HabitとDiaryを保存する必要がある。
-      # 既に登録されているものがある場合は、UPDATEする。
-
+  today_txt=str(datetime.datetime.today().date())
+  try:
+    if request.method=='POST':
       # チェックされたHabitを集計する
       checked_list=request.form.getlist('habit_ids')
       placeholder=', '.join(['?']*len(checked_list))
-      print(placeholder)
       checked_habit_obj=db.execute(
         f'SELECT * FROM habit WHERE id IN ({placeholder})',
         (checked_list)
@@ -29,48 +25,61 @@ def writeTodayDiary():
       regist_datas=[]
       for obj in checked_habit_obj:
         regist_data=[]
-        regist_data.append(str(today))
+        regist_data.append(str(today_txt))
+        # regist_data.append("2026-05-31")
         regist_data.append(obj['id'])
         regist_datas.append(regist_data)
-      # ======================================
-      # 消すやつ
-      db.execute(
-        'DELETE FROM habit_logs'
-      )
-      # ======================================
-      db.executemany(
-        f'INSERT INTO habit_logs ( date, habit_id) VALUES ( ?, ?)',
-        regist_datas
-      )
-      # a=db.execute(
-      #   'SELECT * FROM habit_logs'
-      # ).fetchall()
-      # for b in a:
-      #   print(dict(b))
 
+      today_data=db.execute(
+        f'SELECT * FROM habit_logs WHERE date=(?)',(str(today_txt),)
+      ).fetchall()
+
+      # 既に今日のタスクがあるならUPDATEに切り替える
+      if today_data:
+        for obj in regist_datas:
+          obj.append(str(today_txt))
+        db.executemany(
+          f'UPDATE habit_logs SET date=?, habit_id=? WHERE date=?',regist_datas
+        )
+      else:
+        db.executemany(
+          f'INSERT INTO habit_logs ( date, habit_id) VALUES ( ?, ?)',
+          regist_datas
+        )
+        
+
+      # diary
       diary_text=request.form.get('diary')
-      print(diary_text)
-      # ======================================
-      # 消すやつ
-      db.execute(
-        'DELETE FROM diary'
-      )
-      # ======================================
-      db.execute(
-        "INSERT INTO diary ( date, diary_text) VALUES (?,?)",
-        (today,diary_text)
-      )
+      today_diary_indb=db.execute(
+        f'SELECT * FROM diary WHERE date=?',(str(today_txt),)
+      ).fetchone()
+
+      if today_diary_indb:
+        db.execute(
+          "UPDATE diary SET date=?, diary_text=? WHERE date=?",(today_txt,diary_text,diary_text)
+        )
+      else:
+        db.execute(
+          "INSERT INTO diary ( date, diary_text) VALUES (?,?)",
+          (today_txt,diary_text)
+        )
       db.commit()
+
+      # デバッグ用
+      a=db.execute(
+        'SELECT * FROM habit_logs'
+      ).fetchall()
+      print("-----Debug--------------------------")
+      print("habit_logs")
+      for b in a:
+        print(dict(b))
       diaries=db.execute(
         "SELECT * FROM diary"
       ).fetchall()
+      print("diary")
       for d in diaries:
         print(dict(d))
 
-    except Exception as e:
-      print(str(e))
-
-  try:
   # ==================================
   # 今日表示する必要があるHabitを抽出する。（未）
   # ==================================
@@ -79,7 +88,7 @@ def writeTodayDiary():
     ).fetchall()
     today_log=db.execute(
       'SELECT habit_id FROM habit_logs WHERE date == ?',
-      [str(today)]
+      [str(today_txt)]
     ).fetchall()
     ids=[]
     for id in today_log:
--- memo.md
+++ memo.md
@@ -6,5 +6,5 @@ flask --app flaskr init-db
 uv run -m
 python -m
 
-today.pyで入力した際に、すでに今日のデータが存在した場合、追加ではなくUPDATEもしくは、削除する機能を追加する必要がある
 また、delete機能がないため、追加する
+3日おきに画面に表示される機能が必要
